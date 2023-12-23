e1f8f27a2374d59f56fd8acfd777d2cc0189282f
Saputra <saputra.uta50@gmail.com>
2023-12-23 19:55:11 +0800

    Front-end: Login Page

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  text: string;
+}
+
+export default function CustomButton({ text }: Props) {
+  return (
+    <button
+      type="submit"
+      className="w-full bg-slate-900 text-white rounded-lg px-10 py-2 font-bold tracking-wider text-sm"
+    >
+      {text}
+    </button>
+  );
+}
--- src/components/InputLogin.tsx
+++ src/components/InputLogin.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import useInput from "@/hooks/useInput";
+import CustomButton from "./CustomButton";
 
 export default function InputLogin() {
+  const [email, onEmailChange] = useInput("");
+  const [password, onPasswordChange] = useInput("");
   return (
-    <div>InputLogin</div>
-  )
+    <div className="flex flex-col items-center justify-center w-5/6 gap-2">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold tracking-wider text-slate-900">
+          Sign in
+        </h1>
+        <p className="text-sm sm:text-base md:text-lg lg:text-xl opacity-75 text-slate-900 text-center">
+          Enter your username and password to sign in
+        </p>
+      </div>
+      <form className="flex flex-col items-center justify-center gap-2 w-full mt-5 max-w-2xl">
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="w-full border-2 border-slate-900 p-1 rounded-lg focus:outline-none text-sm sm:text-base"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="w-full border-2 border-slate-900 p-1 rounded-lg focus:outline-none mb-5 text-sm sm:text-base"
+          placeholder="Password"
+        />
+        <CustomButton text="Login" />
+      </form>
+    </div>
+  );
 }
