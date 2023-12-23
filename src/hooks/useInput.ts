386f3550c4f0bac536ca054e9f897902677c3026
Saputra <saputra.uta50@gmail.com>
2023-12-23 19:20:34 +0800

    Second commit

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent, useState } from "react";
+
+export default function useInput(defaultValue = "") {
+  const [value, setvalue] = useState(defaultValue);
+  const onValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setvalue(event.target.value);
+  };
+  return [value, onValueChangeHandler];
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
 
-export default function index() {
+
+export default function login() {
   return (
-    <div>index</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-xl font-bold tracking-wider text-slate-900">
+          Sign in
+        </h1>
+        <p className="text-sm opacity-75 text-slate-900">
+          Enter your username and password to sign in
+        </p>
+      </div>
+    </div>
+  );
 }
